dc3bee838a3b483ee66f1cd6c608e450764507e2
Walter Smuts <smuts.walter@gmail.com>
2021-04-30 22:41:55 +0200

    Change String args to str

--- set-1/src/lib.rs
+++ set-1/src/lib.rs
@@ -1,20 +1,21 @@
-pub fn hex_to_base64(hex: String) -> String {
+pub fn hex_to_base64(hex: &str) -> String {
     base64::encode(hex::decode(hex).unwrap())
 }
 
-pub fn fixed_xor(a: String, b: String) -> String {
+pub fn fixed_xor(a: &str, b: &str) -> String {
     let a = hex::decode(a.as_bytes()).unwrap();
     let b = hex::decode(b.as_bytes()).unwrap();
     let z: Vec<u8> = a.iter().zip(b.iter()).map(|(&x1, &x2)| x1 ^ x2).collect();
     hex::encode(z)
 }
 
-pub fn single_char_xor_decrypt(cypher: String, c: u8) -> String {
+pub fn single_char_xor_decrypt(cypher: &str, c: u8) -> String {
     let mut key = vec![];
     for _ in 0..hex::encode(&cypher).len() {
         key.push(c);
     }
-    String::from_utf8(hex::decode(fixed_xor(cypher, hex::encode(key))).unwrap()).unwrap_or_default()
+    String::from_utf8(hex::decode(fixed_xor(cypher, &hex::encode(key))).unwrap())
+        .unwrap_or_default()
 }
 
 pub fn get_english_score(s: &str) -> usize {
@@ -33,7 +34,7 @@ pub fn get_english_score(s: &str) -> usize {
     i
 }
 
-pub fn single_char_xor_break(cypher: String) -> String {
+pub fn single_char_xor_break(cypher: &str) -> String {
     let mut max = 0;
     let mut best = String::default();
 
@@ -50,7 +51,7 @@ pub fn single_char_xor_break(cypher: String) -> String {
 #[test]
 fn challenge_1() {
     assert_eq!(
-        hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string()),
+        hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"),
         "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
     );
 }
@@ -59,8 +60,8 @@ fn challenge_1() {
 fn challenge_2() {
     assert_eq!(
         fixed_xor(
-            "1c0111001f010100061a024b53535009181c".to_string(),
-            "686974207468652062756c6c277320657965".to_string(),
+            "1c0111001f010100061a024b53535009181c",
+            "686974207468652062756c6c277320657965",
         ),
         "746865206b696420646f6e277420706c6179",
     );
@@ -70,7 +71,7 @@ fn challenge_2() {
 fn challenge_3() {
     assert_eq!(
         single_char_xor_break(
-            "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".to_string()
+            "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
         ),
         "Cooking MC's like a pound of bacon",
     );
