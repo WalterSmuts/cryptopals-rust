63f84b4ac679ac42900c36304bea0203000d490b
Walter Smuts <smuts.walter@gmail.com>
2021-04-30 09:50:40 +0200

    Done set 1 challenge 1

--- set-1/src/lib.rs
+++ set-1/src/lib.rs
@@ -1,7 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub fn hex_to_base64(hex: String) -> String {
+    base64::encode(hex::decode(hex).unwrap())
+}
+
+#[test]
+fn challenge_1() {
+    assert_eq!(
+        hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string()),
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+    );
 }
