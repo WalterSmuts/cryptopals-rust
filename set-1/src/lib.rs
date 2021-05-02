521bbb81ed8c5e54f7db553094b973c23aba8de7
Walter Smuts <smuts.walter@gmail.com>
2021-05-02 16:10:08 +0200

    Removed unnecessary cloning

--- set-1/src/lib.rs
+++ set-1/src/lib.rs
@@ -4,16 +4,16 @@ pub fn hex_to_base64(hex: &str) -> String {
     base64::encode(hex::decode(hex).unwrap())
 }
 
-pub fn fixed_xor(a: Vec<u8>, b: Vec<u8>) -> Vec<u8> {
+pub fn fixed_xor(a: &Vec<u8>, b: &Vec<u8>) -> Vec<u8> {
     a.iter().zip(b.iter()).map(|(&x1, &x2)| x1 ^ x2).collect()
 }
 
-pub fn single_char_xor_decrypt(cypher: Vec<u8>, c: u8) -> Vec<u8> {
+pub fn single_char_xor_decrypt(cypher: &Vec<u8>, c: u8) -> Vec<u8> {
     let mut key = vec![];
     for _ in 0..cypher.len() {
         key.push(c);
     }
-    fixed_xor(cypher, key)
+    fixed_xor(&cypher, &key)
 }
 
 pub fn get_english_score(s: &str) -> usize {
@@ -32,14 +32,13 @@ pub fn get_english_score(s: &str) -> usize {
     i
 }
 
-pub fn most_likely_single_char_xor(cypher: Vec<u8>) -> u8 {
+pub fn most_likely_single_char_xor(cypher: &Vec<u8>) -> u8 {
     let mut max = 0;
     let mut best = 0;
 
     for byte in 0..u8::MAX {
-        let decrypted_candidate = single_char_xor_decrypt(cypher.clone(), byte);
-        let score =
-            get_english_score(&String::from_utf8(decrypted_candidate.clone()).unwrap_or_default());
+        let decrypted_candidate = single_char_xor_decrypt(cypher, byte);
+        let score = get_english_score(&String::from_utf8(decrypted_candidate).unwrap_or_default());
         if score > max {
             max = score;
             best = byte;
@@ -48,8 +47,8 @@ pub fn most_likely_single_char_xor(cypher: Vec<u8>) -> u8 {
     best
 }
 
-pub fn single_char_xor_break(cypher: Vec<u8>) -> Vec<u8> {
-    let byte = most_likely_single_char_xor(cypher.clone());
+pub fn single_char_xor_break(cypher: &Vec<u8>) -> Vec<u8> {
+    let byte = most_likely_single_char_xor(cypher);
     single_char_xor_decrypt(cypher, byte)
 }
 
@@ -57,7 +56,7 @@ pub fn multi_string_xor_detection(candidates: Vec<Vec<u8>>) -> Vec<u8> {
     let mut max = 0;
     let mut best = Vec::default();
     for candidate in candidates {
-        let decrypted_candidate = single_char_xor_break(candidate.clone());
+        let decrypted_candidate = single_char_xor_break(&candidate);
         let score = get_english_score(&String::from_utf8(decrypted_candidate).unwrap_or_default());
         if score > max {
             max = score;
@@ -67,16 +66,16 @@ pub fn multi_string_xor_detection(candidates: Vec<Vec<u8>>) -> Vec<u8> {
     best
 }
 
-pub fn encrypt_repeating_key_xor(plain_text: Vec<u8>, key: Vec<u8>) -> Vec<u8> {
+pub fn encrypt_repeating_key_xor(plain_text: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
     let mut repeating_key = Vec::default();
     while repeating_key.len() + key.len() < plain_text.len() {
         repeating_key.append(&mut key.clone());
     }
     repeating_key.append(&mut key[..plain_text.len() - repeating_key.len()].to_vec());
-    fixed_xor(plain_text, repeating_key)
+    fixed_xor(plain_text, &repeating_key)
 }
 
-pub fn hamming_distance(a: Vec<u8>, b: Vec<u8>) -> u32 {
+pub fn hamming_distance(a: &Vec<u8>, b: &Vec<u8>) -> u32 {
     fixed_xor(a, b)
         .iter()
         .fold(0, |acc, x| acc + x.count_ones())
@@ -89,7 +88,7 @@ pub fn get_most_likely_key_sizes(cypher: &Vec<u8>, amount: usize) -> Vec<usize> 
         for idx in 0..(cypher.len() / MAX_KEY_SIZE / 2) {
             let a = &cypher[idx * keysize..(idx + 1) * keysize];
             let b = &cypher[(idx + 1) * keysize..(idx + 2) * keysize];
-            hamming_distance_sum += hamming_distance(a.to_vec(), b.to_vec());
+            hamming_distance_sum += hamming_distance(&a.to_vec(), &b.to_vec());
         }
         let normalized_hamming_distance = hamming_distance_sum as f64 / keysize as f64;
         contenders.push((keysize, normalized_hamming_distance));
@@ -103,7 +102,7 @@ pub fn get_most_likely_key(cypher: &Vec<u8>, size: usize) -> Vec<u8> {
     for start in 0..size {
         let sub_cypher = &cypher[start..];
         let sub_cypher = sub_cypher.to_vec().into_iter().step_by(size).collect();
-        key.push(most_likely_single_char_xor(sub_cypher));
+        key.push(most_likely_single_char_xor(&sub_cypher));
     }
     key
 }
@@ -112,8 +111,8 @@ pub fn variable_length_xor_break(cypher: Vec<u8>) -> Vec<u8> {
     let sizes = get_most_likely_key_sizes(&cypher, 1);
     let mut candidates = Vec::new();
     for size in sizes {
-        let key = get_most_likely_key(&cypher, size);
-        candidates.push(encrypt_repeating_key_xor(cypher.clone(), key.clone()));
+        let mut key = get_most_likely_key(&cypher, size);
+        candidates.push(encrypt_repeating_key_xor(&cypher, &mut key));
     }
     candidates.sort_by(|a, b| {
         let a = get_english_score(&String::from_utf8(a.to_vec()).unwrap_or_default());
@@ -136,8 +135,8 @@ fn challenge_1() {
 fn challenge_2() {
     assert_eq!(
         hex::encode(fixed_xor(
-            hex::decode("1c0111001f010100061a024b53535009181c").unwrap(),
-            hex::decode("686974207468652062756c6c277320657965").unwrap(),
+            &hex::decode("1c0111001f010100061a024b53535009181c").unwrap(),
+            &hex::decode("686974207468652062756c6c277320657965").unwrap(),
         )),
         "746865206b696420646f6e277420706c6179",
     );
@@ -147,7 +146,7 @@ fn challenge_2() {
 fn challenge_3() {
     assert_eq!(
         String::from_utf8(single_char_xor_break(
-            hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+            &hex::decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
                 .unwrap()
         ))
         .unwrap(),
@@ -166,11 +165,11 @@ fn challenge_4() {
             .collect(),
     );
     assert_eq!(
-        hex::encode(decrypted.clone()),
+        hex::encode(&decrypted),
         "7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f",
     );
     assert_eq!(
-        String::from_utf8(single_char_xor_break(decrypted)).unwrap(),
+        String::from_utf8(single_char_xor_break(&decrypted)).unwrap(),
         "Now that the party is jumping\n",
     );
 }
@@ -179,8 +178,8 @@ fn challenge_4() {
 fn challenge_5() {
     assert_eq!(
         hex::encode(encrypt_repeating_key_xor(
-            "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".as_bytes().to_vec(),
-            "ICE".as_bytes().to_vec(),
+            &"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".as_bytes().to_vec(),
+            &"ICE".as_bytes().to_vec(),
         )),
         "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f",
     );
@@ -190,8 +189,8 @@ fn challenge_5() {
 fn hamming_distance_test() {
     assert_eq!(
         hamming_distance(
-            "this is a test".as_bytes().to_vec(),
-            "wokka wokka!!!".as_bytes().to_vec()
+            &"this is a test".as_bytes().to_vec(),
+            &"wokka wokka!!!".as_bytes().to_vec()
         ),
         37
     );
