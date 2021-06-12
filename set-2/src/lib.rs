3cab50c4f88c46c661ef3b39fdd2164f7482ab93
Walter Smuts <smuts.walter@gmail.com>
2021-06-12 09:44:33 +0200

    Done set 2 challenge 9

--- set-2/src/lib.rs
+++ set-2/src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub fn pad_pkcs_7(mut input: Vec<u8>, block_size: usize) -> Vec<u8> {
+    let num_pad_bytes = block_size - (input.len() % block_size);
+    input.append(&mut vec![num_pad_bytes as u8; num_pad_bytes]);
+    input
+}
+
+#[test]
+fn challenge_9() {
+    assert_eq!(
+        pad_pkcs_7(b"YELLOW SUBMARINE".to_vec(), 20),
+        b"YELLOW SUBMARINE\x04\x04\x04\x04".to_vec()
+    );
 }
